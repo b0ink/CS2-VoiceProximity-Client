1c36691bed729edc9b8027f3f888df8c144a1180
b0ink <40929320+b0ink@users.noreply.github.com>
2025-04-20 15:36:09 +1000

    chore: add env variable types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,9 @@
 
 interface ImportMetaEnv {
   VITE_SOCKET_URL: string;
+  VITE_MEDIA_SAMPLERATE: number;
+  VITE_MEDIA_SAMPLESIZE: number;
+  VITE_USE_TURN_CONFIG: boolean;
 }
 
 interface ImportMeta {
