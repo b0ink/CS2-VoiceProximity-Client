672c1fabbb40550422e857e30074c4d4797588e7
b0ink <40929320+b0ink@users.noreply.github.com>
2025-04-29 16:33:46 +1000

    chore: remove unused vars

--- src/main/ipc-handlers.ts
+++ src/main/ipc-handlers.ts
@@ -3,12 +3,10 @@ import Store from 'electron-store';
 
 const store = new Store();
 
-ipcMain.handle('get-store-value', async (event, key: string) => {
-  console.log(event);
+ipcMain.handle('get-store-value', async (_event, key: string) => {
   return store.get(key);
 });
 
-ipcMain.handle('set-store-value', async (event, key: string, value: any) => {
-  console.log(event);
+ipcMain.handle('set-store-value', async (_event, key: string, value: any) => {
   store.set(key, value);
 });
