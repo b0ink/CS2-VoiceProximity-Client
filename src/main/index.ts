a773a8288261dc5688f3f4e9011122c3e7b7f923
b0ink <40929320+b0ink@users.noreply.github.com>
2025-05-01 00:02:00 +1000

    fix: remove duplicate window instance

--- src/main/index.ts
+++ src/main/index.ts
@@ -47,6 +47,15 @@ function createWindow(): void {
     },
   });
 
+  mainWindow.on('closed', () => {
+    try {
+      mainWindow?.destroy();
+      // If we open any other windows we need to destroy them here
+    } catch {
+      /* empty */
+    }
+  });
+
   mainWindowState.manage(mainWindow);
 
   mainWindow.on('ready-to-show', () => {
@@ -113,52 +122,47 @@ if (!gotTheLock) {
     }
   });
 
-  // Create mainWindow, load the rest of the app, etc...
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
   app.whenReady().then(() => {
+    // Set app user model id for windows
+    electronApp.setAppUserModelId('com.electron');
+
+    // Default open or close DevTools by F12 in development
+    // and ignore CommandOrControl + R in production.
+    // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+    app.on('browser-window-created', (_, window) => {
+      optimizer.watchWindowShortcuts(window);
+    });
+
+    // IPC test
+    ipcMain.on('ping', () => console.log('pong'));
+
     createWindow();
+
+    app.on('activate', function () {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) createWindow();
+      session.fromPartition('default').setPermissionRequestHandler((_webContents, permission, callback) => {
+        const allowedPermissions = ['audioCapture']; // Full list here: https://developer.chrome.com/extensions/declare_permissions#manifest
+        console.log('permission requested ', permission);
+        if (allowedPermissions.includes(permission)) {
+          callback(true); // Approve permission request
+        } else {
+          console.error(
+            `The application tried to request permission for '${permission}'. This permission was not whitelisted and has been blocked.`,
+          );
+
+          callback(false); // Deny
+        }
+      });
+    });
+
+    // checkSteamAuthentication();
   });
 }
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron');
-
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window);
-  });
-
-  // IPC test
-  ipcMain.on('ping', () => console.log('pong'));
-
-  createWindow();
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    session.fromPartition('default').setPermissionRequestHandler((_webContents, permission, callback) => {
-      const allowedPermissions = ['audioCapture']; // Full list here: https://developer.chrome.com/extensions/declare_permissions#manifest
-      console.log('permission requested ', permission);
-      if (allowedPermissions.includes(permission)) {
-        callback(true); // Approve permission request
-      } else {
-        console.error(
-          `The application tried to request permission for '${permission}'. This permission was not whitelisted and has been blocked.`,
-        );
-
-        callback(false); // Deny
-      }
-    });
-  });
-
-  // checkSteamAuthentication();
-});
 
 const checkSteamAuthentication = () => {
   const steamId = store.get('steamId');
@@ -201,6 +205,7 @@ async function getSteamId() {
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
+  mainWindow?.destroy();
   if (process.platform !== 'darwin') {
     app.quit();
   }
