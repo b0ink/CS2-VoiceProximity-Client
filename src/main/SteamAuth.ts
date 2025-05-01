f3c2cfd1c67ed4a9eeef1f88dfc99ae29f1fa516
b0ink <40929320+b0ink@users.noreply.github.com>
2025-05-02 04:35:02 +1000

    chore: remove unused code

--- src/main/SteamAuth.ts
+++ src/main/SteamAuth.ts
@@ -1,19 +1,5 @@
-import { app, BrowserWindow, BrowserWindowConstructorOptions, Event, shell } from 'electron';
+import { app, shell } from 'electron';
 import openid from 'openid';
-import nodeUrl from 'url';
-
-interface SteamOpenIDParams {
-  ns?: string;
-  mode?: string;
-  op_endpoint?: string;
-  claimed_id?: string;
-  identity?: string;
-  return_to?: string;
-  response_nonce?: string;
-  assoc_handle?: string;
-  signed?: string;
-  sig?: string;
-}
 
 const realm = !app.isPackaged
   ? 'http://localhost:3000/'
@@ -23,20 +9,6 @@ const return_url = `${realm}verify-steam`;
 const USE_EXTERNAL_BROWSER = true;
 
 export class SteamAuth {
-  private windowParams: BrowserWindowConstructorOptions;
-  constructor(windowParams: BrowserWindowConstructorOptions = {}) {
-    this.windowParams = {
-      alwaysOnTop: true,
-      autoHideMenuBar: false,
-      skipTaskbar: false,
-      webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false,
-      },
-      ...windowParams,
-    };
-  }
-
   parseOpenIdResponse(openIdResponse: string) {
     const url = new URL(openIdResponse);
     const token = url.searchParams.get('token') || undefined;
