99209c6d8f7dd9f9e1b168f9c3ec876b0185e52f
b0ink <40929320+b0ink@users.noreply.github.com>
2025-05-09 12:46:10 +1000

    feat: add de_vertigo mesh

--- src/renderer/src/maps.ts
+++ src/renderer/src/maps.ts
@@ -1,2 +1,2 @@
 // Available 3D .glb meshes of maps found in the static folder
-export default ['de_dust2', 'de_mirage', 'de_inferno', 'de_nuke'];
+export default ['de_dust2', 'de_mirage', 'de_inferno', 'de_nuke', 'de_vertigo'];
