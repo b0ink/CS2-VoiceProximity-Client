4f9f0fb1d51cd2c21092ef2137f187ffd61f2263
b0ink <40929320+b0ink@users.noreply.github.com>
2025-05-12 01:28:40 +1000

    chore: minify js build

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,16 +1,27 @@
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
+import { BuildEnvironmentOptions } from 'vite';
+
+const defaultBuildOptions: BuildEnvironmentOptions | undefined = {
+  minify: 'terser',
+  terserOptions: {
+    compress: true,
+  },
+};
 
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()],
+    build: { ...defaultBuildOptions },
   },
   preload: {
     plugins: [externalizeDepsPlugin()],
+    build: { ...defaultBuildOptions },
   },
   renderer: {
     plugins: [svelte(), tailwindcss()],
     assetsInclude: ['**/*.glb'], // Add .glb files to the assetsInclude
+    build: { ...defaultBuildOptions },
   },
 });
