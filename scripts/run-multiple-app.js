2fc186d30237c07d55c01a8f7552bd7c48e1387b
b0ink <40929320+b0ink@users.noreply.github.com>
2025-05-19 01:52:27 +1000

    feat: simulate multiple instances of the app

---
+++ scripts/run-multiple-app.js
@@ -0,0 +1,31 @@
+// Use `npm run dev:multi` to simulate 10 applications with 10 unique socket connections
+// Note: each window still requires a valid JWT. Use window.saveAuth()
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { spawn } = require('child_process');
+
+const instances = 10;
+const basePort = 3000;
+const baseInspectPort = 9229;
+
+for (let i = 0; i < instances; i++) {
+  const port = basePort + i;
+  const inspectPort = baseInspectPort + i;
+  const userDir = `/tmp/e${i + 1}`;
+
+  const proc = spawn(
+    'cross-env',
+    [
+      `PORT=${port}`,
+      'electron',
+      `--inspect=${inspectPort}`,
+      './out/main/index.js',
+      `--user-data-dir=${userDir}`,
+    ],
+    { stdio: 'inherit', shell: true },
+  );
+
+  proc.on('exit', (code) => {
+    console.log(`Instance ${i + 1} exited with code ${code}`);
+  });
+}
