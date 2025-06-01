69945758adc80e5ffc8f5eb844ed9808136dad68
b0ink <40929320+b0ink@users.noreply.github.com>
2025-06-01 22:23:06 +1000

    chore: set appid

--- src/main/index.ts
+++ src/main/index.ts
@@ -128,7 +128,7 @@ if (!gotTheLock) {
   // Some APIs can only be used after this event occurs.
   app.whenReady().then(() => {
     // Set app user model id for windows
-    electronApp.setAppUserModelId('com.electron');
+    electronApp.setAppUserModelId('com.boink.cs2.voiceproximity');
 
     // Default open or close DevTools by F12 in development
     // and ignore CommandOrControl + R in production.
