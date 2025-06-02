b5fe38b1bb77eae9ba3e36af5ed4fa428344144c
b0ink <40929320+b0ink@users.noreply.github.com>
2025-06-02 19:44:30 +1000

    fix: update correct import

--- src/renderer/src/store/server-config.ts
+++ src/renderer/src/store/server-config.ts
@@ -1,6 +1,5 @@
 import { writable } from 'svelte/store';
-import type { ServerConfigData } from '@shared/types/api';
-import { DEFAULT_SERVER_CONFIG } from '@shared/types/store/server-config';
+import { DEFAULT_SERVER_CONFIG, type ServerConfigData } from '@shared/types/store/server-config';
 
 const serverConfigStore = writable<ServerConfigData>({
   ...DEFAULT_SERVER_CONFIG,
